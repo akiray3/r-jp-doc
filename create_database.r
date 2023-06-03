f08d96b7454ad88a68591e3c1cae96f23f887828
Akira Yoshida <ayoshida1017@gmail.com>
2023-06-03 17:50:49 +0900

    Update create_database.r

--- create_database.r
+++ create_database.r
@@ -27,7 +27,7 @@ for (pkg in pkgs) {
     fncs <- ls(paste0("package:", pkg))
     addrows <- tibble::tibble(
             pack = pkg, 
-            func = rep(fncs, each = ),
+            func = fncs,
             description = NA,
             usage = NA,
             arguments = NA,
@@ -37,38 +37,41 @@ for (pkg in pkgs) {
             examples = NA
         )
     for (i in 1:length(fncs)) {
-        help_org <- try({
-                utils:::.getHelpFile(help(topic = fncs[i]))
-            }, silent = TRUE)
-        help_html <- capture.output(tools:::Rd2HTML(help_org))
-        sec1n <- grep(x = help_html, pattern = "<h3>Description</h3>")
-        sec2n <- grep(x = help_html, pattern = "<h3>Usage</h3>")
-        sec3n <- grep(x = help_html, pattern = "<h3>Arguments</h3>")
-        sec4n <- grep(x = help_html, pattern = "<h3>Value</h3>")
-        sec5n <- grep(x = help_html, pattern = "<h3>Methods</h3>")
-        sec6n <- grep(x = help_html, pattern = "<h3>See Also</h3>")
-        sec7n <- grep(x = help_html, pattern = "<h3>Examples</h3>")
-        addrows$description[i] <- paste0(help_html[sec1n:(sec2n - 1)], collapse = "")
-        addrows$usage[i] <- paste0(help_html[sec2n:(sec3n - 1)], collapse = "")
-        addrows$arguments[i] <- paste0(help_html[sec3n:(sec4n - 1)], collapse = "")
-        addrows$value[i] <- paste0(help_html[sec4n:(sec5n - 1)], collapse = "")
-        addrows$methods[i] <- paste0(help_html[sec5n:(sec6n - 1)], collapse = "")
-        addrows$see_also[i] <- paste0(help_html[sec6n:(sec7n - 1)], collapse = "")
-        addrows$examples[i] <- paste0(help_html[sec7n:length(text_org)], collapse = "")
-
-
-
-
-        addrows$val[i] <- help_txt
+        trycatch_1 <- try({
+            help_org <- utils:::.getHelpFile(help(topic = fncs[i]))
+            help_html <- capture.output(tools:::Rd2HTML(help_org))
+        }, silent = TRUE)
+        if (class(trycatch_1) == "try-error") {
+            cat(paste0(pkg, "---", fncs[i], "----------ERROR!"), fill = TRUE)
+            next
+        }
+        help_str <- data.frame(
+            item = help_html[grep(x = help_html, pattern = "<h3>")],
+            start = grep(x = help_html, pattern = "<h3>"),
+            stop = NA
+        )
+        help_str$stop <- c(help_str$start[2:nrow(help_str)], length(help_html))
+        sec1 <- as.numeric(help_str[help_str$item == "<h3>Description</h3>", 2:3])
+        sec2 <- as.numeric(help_str[help_str$item == "<h3>Usage</h3>", 2:3])
+        sec3 <- as.numeric(help_str[help_str$item == "<h3>Arguments</h3>", 2:3])
+        sec4 <- as.numeric(help_str[help_str$item == "<h3>Value</h3>", 2:3])
+        sec5 <- as.numeric(help_str[help_str$item == "<h3>Examples</h3>", 2:3])
+        try({
+            addrows$description[i] <- paste0(help_html[sec1[1]:sec1[2]], collapse = "")
+            addrows$usage[i] <- paste0(help_html[sec2[1]:sec2[2]], collapse = "")
+            addrows$arguments[i] <- paste0(help_html[sec3[1]:sec3[2]], collapse = "")
+            addrows$value[i] <- paste0(help_html[sec4[1]:sec4[2]], collapse = "")
+            addrows$examples[i] <- paste0(help_html[sec5[1]:sec5[2]], collapse = "")
+        }, silent = TRUE)
+        cat(paste0(pkg, "---", fncs[i]), fill = TRUE)
     }
-    out <- out %>%
-        dplyr::bind_rows(.,  addrows)
+    out <- dplyr::bind_rows(out,  addrows)
 }
-write_json(out , "help_db_raw.json")
+write_json(out, "help_db_raw.json")
 
-x <- utils:::.getHelpFile(help(topic = "right_join"))
+table(is.na(out$description[out$pack == "graphics"]))
 
-
+out$description[1:100]
 
 test  <- read_html(x = htmlx)
 rvest::html_children(test)
