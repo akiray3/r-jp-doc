f978bdf5e942a4548829d53f30d029eba847b0b3
Akira Yoshida <129808898+akiray3@users.noreply.github.com>
2023-06-05 12:41:35 +0900

    main
    dbの形式を大幅変更

--- create_database.r
+++ create_database.r
@@ -5,81 +5,102 @@ library(package = "tidyverse")
 library(package = "jsonlite")
 library(package = "rvest")
 library(package = "psych")
-
 sessionInfo() 
 pkgs <- c("base", "stats", "graphics", "psych")
-out <- tibble::tibble(
-            pack = pkg, 
-            func = rep(fncs, each = ),
-            description = NA,
-            usage = NA,
-            arguments = NA,
-            value = NA,
-            methods = NA,
-            see_also = NA,
-            examples = NA
+looplist <- data.frame(
+            pack = rep(
+                x = pkgs, 
+                times = sapply(
+                    X = pkgs, 
+                    FUN = function(x){
+                        length(ls(paste0("package:", x)))
+                    }
+                )
+            ),
+            func = unlist(
+                sapply(X = pkgs, FUN = function(x){
+                        ls(paste0("package:", x))
+                    }
+                )
+            )
         )
-ls("package:base")
-ls("package:psych")
-ls("package:dplyr")
-
-for (pkg in pkgs) {
-    fncs <- ls(paste0("package:", pkg))
-    addrows <- tibble::tibble(
-            pack = pkg, 
-            func = fncs,
-            description = NA,
-            usage = NA,
-            arguments = NA,
-            value = NA,
-            methods = NA,
-            see_also = NA,
-            examples = NA
-        )
-    for (i in 1:length(fncs)) {
-        trycatch_1 <- try({
-            help_org <- utils:::.getHelpFile(help(topic = fncs[i]))
-            help_html <- capture.output(tools:::Rd2HTML(help_org))
+outorg <- tibble::tibble(
+        pack = NULL,
+        func = NULL,
+        var = NULL,
+        val = NULL
+    )
+for (i in 1 : nrow(looplist)) {
+    try01 <- try({
+        help_org <- utils:::.getHelpFile(help(topic = looplist$func[i]))
+    }, silent = TRUE)
+    if (class(try01) == "try-error") {
+        try02 <- try({
+            file <- help(topic = looplist$func[i])
+            filenum <- which(basename(file) == looplist$func[i])
+            RdDB <- file.path(dirname(file)[filenum], looplist$pack[i])
+            help_org <- tools:::fetchRdDB(RdDB, looplist$func[i])
         }, silent = TRUE)
-        if (class(trycatch_1) == "try-error") {
-            cat(paste0(pkg, "---", fncs[i], "----------ERROR!"), fill = TRUE)
-            next
+        if (class(try02) == "try-error") {
+            try03 <- try({
+                file <- help(topic = looplist$func[i])
+                filenum <- str_detect(
+                        string = dirname(file), 
+                        pattern = looplist$pack[i]
+                    )
+                if(sum(filenum) > 1) {
+                    filenum <- 1
+                }
+                RdDB <- file.path(dirname(file), looplist$pack[i])
+                help_org <- tools:::fetchRdDB(RdDB[filenum], basename(file)[filenum])
+            }, silent = TRUE)
+            if (class(try03) == "try-error"){
+                if (class(try03) == "try-error") {
+                    cat(paste0("i = ", i), fill = TRUE)
+                    cat(looplist$pack[i], fill = TRUE)
+                    cat(looplist$func[i], fill = TRUE)
+                    print(basename(file))
+                    cat(try03, fill = TRUE)
+                    next
+                }
+            }
         }
-        help_str <- data.frame(
+    }
+    help_html <- capture.output(tools:::Rd2HTML(help_org))
+    startstop <- data.frame(
             item = help_html[grep(x = help_html, pattern = "<h3>")],
             start = grep(x = help_html, pattern = "<h3>"),
             stop = NA
         )
-        help_str$stop <- c(help_str$start[2:nrow(help_str)], length(help_html))
-        sec1 <- as.numeric(help_str[help_str$item == "<h3>Description</h3>", 2:3])
-        sec2 <- as.numeric(help_str[help_str$item == "<h3>Usage</h3>", 2:3])
-        sec3 <- as.numeric(help_str[help_str$item == "<h3>Arguments</h3>", 2:3])
-        sec4 <- as.numeric(help_str[help_str$item == "<h3>Value</h3>", 2:3])
-        sec5 <- as.numeric(help_str[help_str$item == "<h3>Examples</h3>", 2:3])
-        try({
-            addrows$description[i] <- paste0(help_html[sec1[1]:sec1[2]], collapse = "")
-            addrows$usage[i] <- paste0(help_html[sec2[1]:sec2[2]], collapse = "")
-            addrows$arguments[i] <- paste0(help_html[sec3[1]:sec3[2]], collapse = "")
-            addrows$value[i] <- paste0(help_html[sec4[1]:sec4[2]], collapse = "")
-            addrows$examples[i] <- paste0(help_html[sec5[1]:sec5[2]], collapse = "")
-        }, silent = TRUE)
-        cat(paste0(pkg, "---", fncs[i]), fill = TRUE)
+    startstop$stop <- c(
+            startstop$start[2:nrow(startstop)] - 1, 
+            length(help_html)
+        )
+    addrows <- tibble::tibble(
+            pack = looplist$pack[i],
+            func = looplist$func[i],
+            var = startstop$item %>%
+                stringr::str_remove_all(pattern = "<h3>|</h3>") %>%
+                stringr::str_replace_all(pattern = " ", replacement = "_"),
+            val = NA
+        )
+    for (j in 1:nrow(startstop)) {
+        addrows$val[j] <- paste0(
+                help_html[startstop$start[j]:startstop$stop[j]],
+                collapse = ""
+            )
     }
-    out <- dplyr::bind_rows(out,  addrows)
+    outorg <- dplyr::bind_rows(outorg, addrows)
 }
+
+typicalvar <- table(outorg$var)[table(outorg$var) > (nrow(looplist) / 2)]
+out <- outorg %>%
+    dplyr::filter(
+        var %in% names(typicalvar)
+    ) %>%
+    dplyr::group_by(pack, func) %>%
+    tidyr::spread(key = var, value = val) %>%
+    dplyr::select(pack, func, Description, Usage, Arguments, Value,
+        Details, Examples, References, See_Also)
 write_json(out, "help_db_raw.json")
-
-table(is.na(out$description[out$pack == "graphics"]))
-
-out$description[1:100]
-
-test  <- read_html(x = htmlx)
-rvest::html_children(test)
-rvest::html_text(test, trim = FALSE)
-rvest::html_text(test, trim = TRUE)
-rvest::html_node(test, css = "body") %>%
-    rvest::html_text()
-rvest::html_node(test, css = "h3")
-
-html_element(test, css="body") %>%
-　　　　html_text()
+openxlsx::write.xlsx(x = out, file = "help_db_raw.xlsx")
