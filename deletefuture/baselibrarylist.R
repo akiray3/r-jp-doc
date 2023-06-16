eb6103a6df4baed8a1a3ca757d095412acf9b41f
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2023-06-16 21:46:35 +0900

    detail html修正

--- deletefuture/baselibrarylist.R
+++ deletefuture/baselibrarylist.R
@@ -3,9 +3,14 @@ needs(readxl)
 needs(tidyverse)
 dat <- readxl::read_xlsx("/Users/aizawaharuka/Documents/GitHub/r-jp-doc/deletefuture/help_dbコピー関数名用.xlsx")
 head(dat)
-dat %>% mutate(html = func) %>% 
+dat2 <- dat %>% 
+ mutate(tagtxt = func) %>% 
+filter(pack == "stats") 
+ 
+dat2$func <- gsub("[/\\\\?%*:|\"<>]","_",dat2$func)
+dat2%>%
+  mutate(html = func)%>%
   mutate(html = str_remove(string = html,pattern = "\\.")) %>% 
-  mutate(tagtxt = func) %>% 
   mutate(func = paste0("{ id:'",func,"', href:")) %>% 
   mutate(html = paste0("'/detail/base/",html,".html'")) %>% 
   mutate(tagtxt = paste0(",texttag:'",tagtxt,"'}")) %>% 
@@ -16,38 +21,34 @@ dat %>% mutate(html = func) %>%
 
 
 #base関数用　関数紹介のページ作成
-filenamelist <- dat %>%
-  mutate(html = func) %>% 
-  filter(pack == "graphics") %>% 
-  mutate(html = str_remove(string = html,pattern = "\\.")) %>% 
-  select(html) %>% as.list()
+filenamelist <- dat2 %>%
+  filter(pack == "stats") %>% 
+  mutate(func = str_remove(string = func,pattern = "\\.")) %>% 
+  select(func) %>% as.list()%>%unlist()%>%unique()
 
-for (i in filenamelist$html) {
-  content <- paste0("<!DOCTYPE html><head>    <title>",i,"</title>
-                    </head>
-                    <body>    
-                    <div class=\"block\"></div>    
-                    <div
-    class=\"tabs\"
-    style=\"display: flex; transform: translate(0, 0)\"
-  >
-    <button class=\"tab\" onclick=\"openTab(event,'Description') \">
-      Description
-    </button>
-    <button class=\"tab\" onclick=\"openTab(event,'Arguments')\">Arguments</button>
-    <button class=\"tab\" onclick=\"openTab(event,'Value')\">Value</button>
-    <button class=\"tab\" onclick=\"openTab(event,'Details')\">Details</button>
-    <button class=\"tab\" onclick=\"openTab(event,'Examples')\">Examples</button>
-    <button class=\"tab\" onclick=\"openTab(event,'References')\">
-      References
-    </button>
-    <button class=\"tab\" onclick=\"openTab(event,'See_Also')\">See_Also</button>
-  </div>
+for (i in filenamelist) {
+content <- paste0("
+<!DOCTYPE html><head>    <title>", i, "</title>
+ </head>
+ <body>    
+ <div class=\"block\"></div>    
+ <div class=\"tabs\" style=\"display: flex; transform: translate(0, 0)\" >
+   <button class=\"tab\" onclick=\"openTab(event,'Description') \">
+     Description
+   </button>
+   <button class=\"tab\" onclick=\"openTab(event,'Arguments')\">Arguments</button>
+   <button class=\"tab\" onclick=\"openTab(event,'Value')\">Value</button>
+   <button class=\"tab\" onclick=\"openTab(event,'Details')\">Details</button>
+   <button class=\"tab\" onclick=\"openTab(event,'Examples')\">Examples</button>
+   <button class=\"tab\" onclick=\"openTab(event,'References')\">
+     References
+   </button>
+   <button class=\"tab\" onclick=\"openTab(event,'See_Also')\">See_Also</button>
+ </div>
 <div class=\"tabcontents\" style=\"display: block;\"></div>
-  <link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\" />
-  <script src=\"script.js\"></script>
-                    </body>")
-  path <- file.path("/Users/aizawaharuka/Documents/GitHub/r-jp-doc/detail/graphics",paste0(i,".html"))
-  write_lines(content,path)
+<link rel=\"stylesheet\" href=\"../style.css\" type=\"text/css\" />
+<script src=\"../script.js\"></script>
+</body>")    
+  htmlpath <- file.path("/Users/aizawaharuka/Documents/GitHub/r-jp-doc/detail/stats", paste0(i, ".html"))
+  write_lines(content, path = htmlpath)
 }
-
