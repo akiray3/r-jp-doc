7c8e2b728ca27ce2e7350bb44a32022aae499aa8
akiray3 <ayoshida1017@gmail.com>
2023-06-20 14:42:25 +0900

    2023/06/20

--- R/#02_translate_database.r
+++ R/#02_translate_database.r
@@ -4,16 +4,37 @@
 
 library(package = "tidyverse")
 library(package = "jsonlite")
-library(package = "httr")
-library(package = "googleLanguageR")
+library(package = "rvest")
 
 org <- jsonlite::fromJSON(txt = "help_db_raw.json") %>%
     dplyr::as_tibble() %>%
     print()
 
-thistext <- org$Arguments[1]
-startnum <- regexpr(pattern = "<p>", text = thistext)[1] + 3
-stopnum <- regexpr(pattern = "</p>", text = thistext)[1] - 1
+thistext <- org$Arguments[2]
+
+arg_eng_list <- list(NULL)
+length(arg_eng_list) <- nrow(org)
+
+for (i in 1:nrow(org)) {
+  if(is.na(org$Arguments[i])) {
+    next
+  }
+  arg_eng_list[[i]] <- org$Arguments[i] %>%
+    rvest::read_html() %>%
+    rvest::html_nodes("p") %>%
+    as.character() %>%
+    stringr::str_remove_all(pattern = "<p>|</p>")
+}
+
+out1 <- unlist(arg_eng_list)
+length(out1)
+
+write.table(x = out1[1:8000], file = "R/arg_eng_list.txt", sep = ",", quote = F)
+
+
+
+
+
+startnum <- gregexpr(pattern = "<p>", text = thistext)[[1]] + 3
+stopnum <- gregexpr(pattern = "</p>", text = thistext)[[1]] - 1
 transtext <- substring(text = thistext, first = startnum, last = stopnum)
-googleLanguageR::gl_translate(t_string = transtext, target = "jp")
-googleLanguageR::gl_auth(json_file = )
