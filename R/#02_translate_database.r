22bedfc127158e4db991b8f6f9df8eb9520d9205
akiray3 <ayoshida1017@gmail.com>
2023-06-20 09:24:58 +0900

    午前9時24分

--- R/#02_translate_database.r
+++ R/#02_translate_database.r
@@ -5,9 +5,15 @@
 library(package = "tidyverse")
 library(package = "jsonlite")
 library(package = "httr")
+library(package = "googleLanguageR")
 
 org <- jsonlite::fromJSON(txt = "help_db_raw.json") %>%
     dplyr::as_tibble() %>%
     print()
 
-org$Arguments[1]
+thistext <- org$Arguments[1]
+startnum <- regexpr(pattern = "<p>", text = thistext)[1] + 3
+stopnum <- regexpr(pattern = "</p>", text = thistext)[1] - 1
+transtext <- substring(text = thistext, first = startnum, last = stopnum)
+googleLanguageR::gl_translate(t_string = transtext, target = "jp")
+googleLanguageR::gl_auth(json_file = )
