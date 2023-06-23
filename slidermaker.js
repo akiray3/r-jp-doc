06dde220f9957dd84372a98dba8a7be410dc4b65
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2023-06-23 21:32:29 +0900

    slide修正

--- slidermaker.js
+++ slidermaker.js
@@ -5,30 +5,37 @@ for (let b = 0; b < list.length; b++) {
   const jsonfile = new XMLHttpRequest();
   jsonfile.open("GET", path, true);
   jsonfile.send();
-  
+
   jsonfile.onload = function () {
     const jsondata = JSON.parse(jsonfile.responseText);
-    const func_basename = jsondata.map((item) => item.funcname);
-    const func_link = jsondata.map((item) => item.href);
     const slideblock = document.querySelector("#" + list[b] + "_slider");
-
-    for (let i = 0; i < func_basename.length; i++) {
+    for (let i = 0; i < jsondata.length; i++) {
       //base
       const slide = document.createElement("div");
       slide.className = "slick-list";
-      slide.id = func_basename[i];
+      slide.id = jsondata[i].funcname;
       const slide_link = document.createElement("a");
-      slide_link.href = func_link[i];
-      const imgbox = document.createElement("img");
-      imgbox.src = "www/square.svg";
-      imgbox.style.height = "200px";
-      const funcnametxt = document.createElement("p");
-      const fnnametxt = document.createTextNode(func_basename[i]);
-      funcnametxt.appendChild(fnnametxt);
-      slide_link.appendChild(imgbox);
-      slide.appendChild(slide_link);
-      slide.appendChild(funcnametxt);
-      slideblock.appendChild(slide);
+      slide_link.href = jsondata[i].href;
+      if (jsondata[i].svg) {
+        const imgbox = document.createElement("img");
+        imgbox.src = "www/" + jsondata[i].svg;
+        imgbox.style.height = "200px";
+        const funcnametxt = document.createElement("p");
+        const fnnametxt = document.createTextNode(jsondata[i].funcname);
+        funcnametxt.appendChild(fnnametxt);
+        slide_link.appendChild(imgbox);
+        slide.appendChild(slide_link);
+        slide.appendChild(funcnametxt);
+        slideblock.appendChild(slide);
+      } else {
+        const funcnametxt = document.createElement("p");
+        const fnnametxt = document.createTextNode(jsondata[i].funcname);
+        funcnametxt.appendChild(fnnametxt);
+        slide.appendChild(slide_link);
+        slide.appendChild(funcnametxt);
+        slideblock.appendChild(slide);
+      }
+      
     }
   };
 }
