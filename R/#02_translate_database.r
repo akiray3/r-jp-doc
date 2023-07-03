d2a9613640eda74d730d2db67a8c75e14249e3ec
akiray3 <ayoshida1017@gmail.com>
2023-07-03 10:07:10 +0900

    翻訳txtファイルの整理

--- R/#02_translate_database.r
+++ R/#02_translate_database.r
@@ -1,7 +1,4 @@
 #### Script for collecting R help function text.
-# Not needed for web app functionality, but left in case.
-# Set "r-jp-doc" as the working directory.
-
 library(package = "tidyverse")
 library(package = "jsonlite")
 library(package = "rvest")
@@ -9,95 +6,60 @@ library(package = "rvest")
 org <- jsonlite::fromJSON(txt = "help_db_raw.json") %>%
     dplyr::as_tibble() %>%
     print()
-imax <- nrow(org)
-dsc_eng_list <- list(NULL)
-arg_eng_list <- list(NULL)
-dtl_eng_list <- list(NULL)
-length(dsc_eng_list) <- length(arg_eng_list) <- length(dtl_eng_list) <- imax
-names(dsc_eng_list) <- names(arg_eng_list) <- names(dtl_eng_list) <- org$func
 
-for (i in 1:imax) {
-  if (!is.na(org$Description[i])) {
-    dsc_eng_list[[i]] <- org$Description[i] %>%
-      rvest::read_html() %>%
-      rvest::html_nodes("p") %>%
-      as.character() %>%
-      stringr::str_remove_all(pattern = "<p>|</p>")
-  }
-  if (!is.na(org$Arguments[i])) {
-    arg_eng_list[[i]] <- org$Arguments[i] %>%
-      rvest::read_html() %>%
-      rvest::html_nodes("p") %>%
-      as.character() %>%
-      stringr::str_remove_all(pattern = "<p>|</p>")
-  }
-  if (!is.na(org$Details[i])) {
-    dtl_eng_list[[i]] <- org$Details[i] %>%
-      rvest::read_html() %>%
-      rvest::html_nodes("p") %>%
-      as.character() %>%
-      stringr::str_remove_all(pattern = "<p>|</p>")
-  }
+write.table(x = org$Description, file = "R/dsc_eng.txt",
+  sep = ",", quote = FALSE, row.names = FALSE)
+seq_i <- c(seq(1, nrow(org), 500), nrow(org))
+for (i in 2:length(seq_i)) {
+  write.table(
+    x = org$Arguments[seq_i[i - 1]:seq_i[i]],
+    file = paste0("R/arg_eng_", i - 1, ".txt"),
+    sep = ",",
+    quote = FALSE,
+    row.names = FALSE
+  )
+  write.table(
+    x = org$Details[seq_i[i - 1]:seq_i[i]],
+    file = paste0("R/dtl_eng_", i - 1, ".txt"),
+    sep = ",",
+    quote = FALSE,
+    row.names = FALSE
+  )
 }
 
-rep(org$func, times = as.numeric(sapply(arg_eng_list, length)))
-rep(org$func, times = as.numeric(sapply(dtl_eng_list, length)))
-
-length(unlist(dsc_eng_list))
-write.table(x = unlist(dsc_eng_list), file = "R/eng_dsc.txt",
-  sep = ",", quote = FALSE, row.names = FALSE)
-length(unlist(arg_eng_list))
-write.table(x = unlist(arg_eng_list)[1:9000], file = "R/eng_arg_1.txt",
-  sep = ",", quote = FALSE, row.names = FALSE)
-write.table(x = unlist(arg_eng_list)[9001:17480], file = "R/eng_arg_2.txt",
-  sep = ",", quote = FALSE, row.names = FALSE)
-length(unlist(dsc_eng_list))
-write.table(x = unlist(dsc_eng_list), file = "R/eng_dtl.txt",
-  sep = ",", quote = FALSE, row.names = FALSE)
-
-dsc_jpn <- readLines("R/jpn_dsc.txt", skipNul = TRUE)[-1]
-dsc_eng <- readLines("R/eng_dsc.txt", skipNul = TRUE)[-1]
-func_part <- rep(org$func, times = as.numeric(sapply(dsc_eng_list, length)))
-length(dsc_jpn)
-length(func_part)
-
+# desc
+dsc_jpn <- readLines("R/dsc_jpn.txt", skipNul = TRUE)[-1]
+dsc_eng <- readLines("R/dsc_eng.txt", skipNul = TRUE)[-1]
+dsc_fnc <- rep(org$func, times = as.numeric(sapply(dsc_eng_list, length)))
 desc_je <- tibble::tibble(jpn = dsc_jpn, eng = dsc_eng) %>%
   dplyr::filter(eng != "") %>%
-  dplyr::mutate(func = func_part) %>%
+  dplyr::mutate(func = dsc_fnc) %>%
   dplyr::select(func, eng, jpn) %>%
   print()
 org_nrow <- nrow(org)
 for (i in 1:org_nrow) {
-  desc_i <- desc_je %>%
-    dplyr::filter(func == org$func[i])
-  eng_i <- desc_i %>%
-    dplyr::pull(eng) %>%
-    paste0(collapse = "")
-  jpn_i <- desc_i %>%
+  jpn_i <- desc_je %>%
+    dplyr::filter(func == org$func[i]) %>%
     dplyr::pull(jpn) %>%
     paste0(collapse = "")
-  org$Description[i] <- stringr::str_replace(
-      string = org$Description[i],
-      pattern = eng_i,
-      replacement = jpn_i
-    )
-
-
+  org$Description[i] <- paste0("<h3>説明</h3><p>", jpn_i, "</p>")
 }
 
-x <- "Create a contingency table (optionally a sparse matrix) fromcross-classifying factors, usually contained in a data frame,using a formula interface."
-y <- "<h3>Description</h3><p>Create a contingency table (optionally a sparse matrix) fromcross-classifying factors, usually contained in a data frame,using a formula interface.</p>"
-
-str_detect(string = y, pattern = x)
-regexpr(pattern = x, text = y)
-regexpr(pattern = "apple", text = y)
-regexpr(pattern = "<p>", text = y)
-charmatch(x = "<p>", table = y)
-
-startnum <- gregexpr(pattern = "<p>", text = thistext)[[1]] + 3
-stopnum <- gregexpr(pattern = "</p>", text = thistext)[[1]] - 1
-transtext <- substring(text = thistext, first = startnum, last = stopnum)
-
-
-"These unary and binary operators perform arithmetic on numeric orcomplex vectors (or objects which can be coerced to them)."
-"<h3>Description</h3><p>These unary and binary operators perform arithmetic on numeric orcomplex vectors (or objects which can be coerced to them).</p>"
+# arg
+arg_jpn <- c(readLines("R/arg_jpn_1.txt", skipNul = TRUE)[-1],
+    readLines("R/arg_jpn_2.txt", skipNul = TRUE)[-1])
+arg_eng <- c(readLines("R/arg_eng_1.txt", skipNul = TRUE)[-1],
+    readLines("R/arg_eng_2.txt", skipNul = TRUE)[-1])
+arg_fnc <- rep(org$func, times = as.numeric(sapply(arg_eng_list, length)))
+arg_je <- tibble::tibble(jpn = arg_jpn, eng = arg_eng) %>%
+  dplyr::filter(eng != "") %>%
+  dplyr::mutate(func = arg_fnc) %>%
+  dplyr::select(func, eng, jpn) %>%
+  print()
+for (i in 1:org_nrow) {
+  jpn_i <- arg_je %>%
+    dplyr::filter(func == org$func[i]) %>%
+    dplyr::pull(jpn) %>%
+    paste0(collapse = "")
+  org$Description[i] <- paste0("<h3>説明</h3><p>", jpn_i, "</p>")
+}
