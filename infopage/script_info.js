78a6263679bfb34b088196ea82bc979056a4f57c
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2023-07-23 15:37:12 +0900

    base関数の絞り込み

--- infopage/script_info.js
+++ infopage/script_info.js
@@ -65,3 +65,69 @@ request.onload = function () {
     infocontent.appendChild(div);
   }
 };
+function selectFunction(event, tabId) {
+  const allFunc = document.querySelector(".slidermenu_base");
+  allFunc.classList.add("none");
+  request.open("GET", path, true);
+  request.send();
+  request.onload = function () {
+    const jsondata = JSON.parse(request.responseText);
+    const tagname = jsondata.filter((item) => item.tag === tabId);
+    const filtertag = tagname.map((item) => item.funcname);
+
+    const allMenu = document.querySelectorAll(".TagMenu");
+    allMenu.forEach((item) => (item.style.display = "none"));
+    const tagmenu = document.getElementById(`${tabId}Menu`);
+    tagmenu.style.display = "block";
+    
+    filtertag.forEach((element) => {
+      const div = document.createElement("div");
+      div.className = "lineitem";
+      div.id = element;
+      div.style.display = "inline-block";
+      div.style.fill = "none";
+
+      const div2 = document.createElement("div");
+      div2.className = "namebox";
+
+      const atag = document.createElement("a");
+      atag.href = `../detail/detailpage.html`;
+      atag.addEventListener("click", (event) => {
+        event.preventDefault();
+        window.location.href = ` ../detail/detailpage.html?keyword=${element}`;
+      });
+
+      const txttag = document.createElement("button");
+      txttag.className = "ToDetailButton";
+
+      txttag.style.backgroundColor = "#00000000";
+      txttag.style.border = "none";
+
+      if (WindowSize <= 395) {
+        if (element.length < 15) {
+          txttag.style.fontSize = "39px";
+        } else if (element.length >= 15 && element.length < 20) {
+          txttag.style.fontSize = "35px";
+        } else if (element.length >= 20 && element.length < 35) {
+          txttag.style.fontSize = "30px";
+        } else {
+          txttag.style.fontSize = "25px";
+        }
+      } else {
+        if (element.length < 18) {
+          txttag.style.fontSize = "26px";
+        } else {
+          txttag.style.fontSize = "20px";
+        }
+      }
+
+      txttag.style.fontFamily = "ArialRoundedMTBold";
+      const txtnode = document.createTextNode(element);
+      txttag.appendChild(txtnode);
+      atag.appendChild(txttag);
+      div2.appendChild(atag);
+      div.appendChild(div2);
+      tagmenu.appendChild(div);
+    });
+  };
+}
