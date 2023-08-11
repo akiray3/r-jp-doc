8dbff10792db4e0ddb0e5bb6770a4388968a34a8
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2023-08-11 22:23:14 +0900

    description挿入

--- infopage/script_info.js
+++ infopage/script_info.js
@@ -22,149 +22,128 @@ const request = new XMLHttpRequest();
 request.open("GET", path, true);
 request.send();
 
-request.onload = function () {
-  const jsondata = JSON.parse(request.responseText);
-  const funcname = jsondata.map((item) => item.funcname);
-  const href = jsondata.map((item) => item.href);
-  const infocontent = document.getElementById(titleid + "info");
-  for (let i = 0; i < funcname.length; i++) {
-    const div = document.createElement("div");
-    div.className = "lineitem";
-    div.id = funcname[i];
-    div.style.display = "inline-block";
-    div.style.fill = "none";
+function groupBy(obj, key) {
+  return obj.reduce((abc, xyz) => {
+    const match = xyz[key];
+    const group = abc[match] ?? [];
 
-    const div2 = document.createElement("div");
-    div2.className = "namebox";
-
-    const atag = document.createElement("a");
-    atag.href = `../detail/detailpage.html`;
-    atag.addEventListener("click", (event) => {
-      event.preventDefault();
-      window.location.href = ` ../detail/detailpage.html?keyword=${funcname[i]}`;
-    });
-
-    const txttag = document.createElement("button");
-    txttag.className = "ToDetailButton";
-
-    txttag.style.border = "none";
-
-    if (WindowSize <= 395) {
-      if (funcname[i].length < 15) {
-        txttag.style.fontSize = "39px";
-      } else if (funcname[i].length >= 15 && funcname[i].length < 20) {
-        txttag.style.fontSize = "35px";
-      } else if (funcname[i].length >= 20 && funcname[i].length < 35) {
-        txttag.style.fontSize = "30px";
-      } else {
-        txttag.style.fontSize = "25px";
-      }
-    } else {
-      if (funcname[i].length < 18) {
-        txttag.style.fontSize = "26px";
-      } else {
-        txttag.style.fontSize = "20px";
-      }
-    }
-    const txtnode = document.createTextNode(funcname[i]);
-
-    txttag.appendChild(txtnode);
-    atag.appendChild(txttag);
-    div2.appendChild(atag);
-    div.appendChild(div2);
-    infocontent.appendChild(div);
-  }
-};
-function selectFunction(event, tabId) {
-  const allFunc = document.querySelector(".slidermenu_base");
-  allFunc.classList.add("none");
-  request.open("GET", path, true);
-  request.send();
-  request.onload = function () {
-    const jsondata = JSON.parse(request.responseText);
-    const tagname = jsondata.filter((item) => item.tag === tabId);
-    const filtertag = tagname.map((item) => item.funcname);
-
-    const allMenu = document.querySelectorAll(".TagMenu");
-    allMenu.forEach((item) => (item.style.display = "none"));
-    const tagmenu = document.getElementById(`${tabId}Menu`);
-    tagmenu.style.display = "block";
-
-    filtertag.forEach((element) => {
-      const div = document.createElement("div");
-      div.className = "lineitem";
-      div.id = element;
-      div.style.display = "inline-block";
-      div.style.fill = "none";
-
-      const div2 = document.createElement("div");
-      div2.className = "namebox";
-
-      const atag = document.createElement("a");
-      atag.href = `../detail/detailpage.html`;
-      atag.addEventListener("click", (event) => {
-        event.preventDefault();
-        window.location.href = ` ../detail/detailpage.html?keyword=${element}`;
-      });
-
-      const txttag = document.createElement("button");
-      txttag.className = "ToDetailButton";
-
-      txttag.style.border = "none";
-
-      if (WindowSize <= 395) {
-        if (element.length < 15) {
-          txttag.style.fontSize = "39px";
-        } else if (element.length >= 15 && element.length < 20) {
-          txttag.style.fontSize = "35px";
-        } else if (element.length >= 20 && element.length < 35) {
-          txttag.style.fontSize = "30px";
-        } else {
-          txttag.style.fontSize = "25px";
-        }
-      } else {
-        if (element.length < 18) {
-          txttag.style.fontSize = "26px";
-        } else {
-          txttag.style.fontSize = "20px";
-        }
-      }
-
-      txttag.style.fontFamily = "ArialRoundedMTBold";
-      const txtnode = document.createTextNode(element);
-      txttag.appendChild(txtnode);
-      atag.appendChild(txttag);
-      div2.appendChild(atag);
-      div.appendChild(div2);
-      tagmenu.appendChild(div);
-    });
-  };
+    return { ...abc, [match]: [...group, xyz] };
+  }, {});
 }
 
+fetch("../help_db_jpn_main.json")
+  .then((response) => response.json())
+  .then((jsondata) => {
+    const packFilter = jsondata.filter((item) => item.pack === titleid);
+    const groupbyDes = groupBy(packFilter, "Description");
 
-function pageTopAnime(){
+    const keys = Object.keys(groupbyDes);
+
+    keys.forEach((element) => {
+      const keyframe = document.createElement("div");
+      keyframe.className = "keyframe";
+      const filterDes = packFilter.filter(
+        (item) => item.Description === element
+      );
+      const FilterDesFunclist = filterDes.map((item) => item.func);
+      FilterDesFunclist.forEach((element) => {
+        const div = document.createElement("div");
+        div.className = "lineitem";
+        div.id = element;
+        div.style.display = "inline-block";
+        div.style.fill = "none";
+
+        const div2 = document.createElement("div");
+        div2.className = "namebox";
+
+        const atag = document.createElement("a");
+        atag.href = `../detail/detailpage.html`;
+        atag.addEventListener("click", (event) => {
+          event.preventDefault();
+          window.location.href = ` ../detail/detailpage.html?keyword=${element}`;
+        });
+
+        const txttag = document.createElement("button");
+        txttag.className = "ToDetailButton";
+
+        txttag.style.border = "none";
+
+        if (WindowSize <= 395) {
+          if (element.length < 15) {
+            txttag.style.fontSize = "39px";
+          } else if (element.length >= 15 && element.length < 20) {
+            txttag.style.fontSize = "35px";
+          } else if (element.length >= 20 && element.length < 35) {
+            txttag.style.fontSize = "30px";
+          } else {
+            txttag.style.fontSize = "25px";
+          }
+        } else {
+          if (element.length < 18) {
+            txttag.style.fontSize = "26px";
+          } else {
+            txttag.style.fontSize = "20px";
+          }
+        }
+        const txtnode = document.createTextNode(element);
+        fetch(`${titleid}.json`)
+          .then((response) => response.json())
+          .then((svgjsondata) => {
+            const svgdata = svgjsondata.find(
+              (item) => item.funcname === element
+            );
+
+            if (svgdata.svg) {
+              const image = document.createElement("img");
+              image.className = "svg";
+              image.src = `../www/${svgdata.svg}`;
+              txttag.appendChild(image);
+            } else {
+            }
+          });
+        txttag.appendChild(txtnode);
+        atag.appendChild(txttag);
+        div2.appendChild(atag);
+        div.appendChild(div2);
+        keyframe.appendChild(div);
+        const sec_slide = document.querySelector(".sec_slide");
+        sec_slide.appendChild(keyframe);
+
+        
+      });
+      const descriptionBox = document.createElement("div");
+        descriptionBox.className = "descriptionBox";
+        descriptionBox.textContent = element;
+        keyframe.appendChild(descriptionBox);
+    });
+
+  });
+
+function pageTopAnime() {
   let scroll = $(window).scrollTop();
-  if(scroll >= 300){
-    $('#pageTop').removeClass('DownMove');
-    $('#pageTop').addClass('UpMove');
-  }else{
-    if($('#pageTop').hasClass('UpMove')){
-      $('#pageTop').removeClass('UpMove');
-      $('#pageTop').addClass('DownMove');
+  if (scroll >= 300) {
+    $("#pageTop").removeClass("DownMove");
+    $("#pageTop").addClass("UpMove");
+  } else {
+    if ($("#pageTop").hasClass("UpMove")) {
+      $("#pageTop").removeClass("UpMove");
+      $("#pageTop").addClass("DownMove");
     }
   }
 }
 $(window).scroll(function () {
-pageTopAnime();
+  pageTopAnime();
 });
 
-$(window).on('load',function(){
+$(window).on("load", function () {
   pageTopAnime();
-})
-$('#pageTop a').click(function(){
-  $('body,html').animate({
-    scrollTop:0
-  },500);
+});
+$("#pageTop a").click(function () {
+  $("body,html").animate(
+    {
+      scrollTop: 0,
+    },
+    500
+  );
   return false;
-  }
-);
+});
