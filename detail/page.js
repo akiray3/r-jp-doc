f69ddb099cc28e662537d80330ac45d89115bab6
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2023-12-25 22:32:28 +0900

    メール送信追加

--- detail/page.js
+++ detail/page.js
@@ -455,3 +455,25 @@ fetch(errorFilePath)
       option.appendChild(optionJp);
     }
   });
+
+
+  const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_njv2rvq';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
